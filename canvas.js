acb8c1d42c6ab7345fb1740fd04f32cbc3be0bf5
ramon kamibayashi <ramon.kamiabayashi@mores.es>
2012-10-19 20:02:16 +0200

    segundo commit

---
+++ canvas.js
@@ -0,0 +1,11 @@
+$("#canvas").drawImage({
+  source: "imgs/salon01.png",
+  x: 0, y: 0,
+  fromCenter: false
+});
+
+$("#canvas").drawImage({
+  source: "imgs/nemo.png",
+  scale: 0.4,
+  x: 400, y: 200,
+});
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Test de canvas</title>
+	<<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+
+<body>
+
+	<canvas id="canvas" width="800" height="550"></canvas>
+
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.2.js"></script>
+	<script type="text/javascript" src="jcanvas.min.js"></script>
+	<script type="text/javascript" src="canvas.js"></script>
+</body>
+
+</html>
